8b36bdf051a0e5c72354e2adfd8be02e1d7d733b
CharlesLee-23 <Coleman.hallom@Spray.com>
2021-08-05 15:21:06 -0500

    Changes Made

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,24 @@ namespace Reservator
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(591, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 93);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1007, 663);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -43,6 +54,8 @@ namespace Reservator
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
